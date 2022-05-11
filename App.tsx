b25e18e5522d78ab402ffd587e97d9067d95e09f
taZik616 <taZik-616@yandex.ru>
2022-05-11 21:25:45 +0500

    first commit

---
+++ App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { SafeAreaView, View } from 'react-native'
+// @ts-expect-error
+import { Server_URL, Application_ID } from 'react-native-dotenv'
+
+const App = () => {
+  return <SafeAreaView />
+}
+
+export default App
